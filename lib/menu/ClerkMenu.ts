80c16206eb289b863cc95aa34c9b57978ab80d60
unknown <shemregidor6@gmail.com>
2026-06-11 23:20:33 +0800

    create clerk menu items

---
+++ lib/menu/ClerkMenu.ts
@@ -0,0 +1,43 @@
+import {
+  LayoutDashboard,
+  Users,
+  Settings,
+  FileText,
+  Archive,
+  BarChart3,
+  Download,
+} from "lucide-react";
+
+export const adminMenu = [
+  {
+    name: "Dashboard",
+    path: "/admin/dashboard",
+    icon: LayoutDashboard,
+  },
+  {
+    name: "Registration",
+    path: "/admin/registration",
+    icon: FileText,
+  },
+  {
+    name: "Records",
+    path: "/admin/records",
+    icon: Archive,
+  },
+
+  {
+    name: "Analytics",
+    path: "/admin/analytics",
+    icon: BarChart3,
+  },
+  {
+    name: "Forms",
+    path: "/admin/forms",
+    icon: Download,
+  },
+  {
+    name: "Settings",
+    path: "/admin/settings",
+    icon: Settings,
+  },
+];
